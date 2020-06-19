9704dd72dea50cb211a943e7ac94fd2c432f0b7a
smiface <smiface@mail.com>
2020-06-20 02:22:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gh-pages/precache-manifest.e5e6aae086d706389f0e677cef9e56dc.js"
+  "/gh-pages/precache-manifest.079c62b5753de1544d208d01c46699c6.js"
 );
 
 self.addEventListener('message', (event) => {
